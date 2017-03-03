4b8ebb22ea4cac9f51b31a834b583e1122b3ec1f
Carlos A. Vaca Morales <carlosv@rain.agency>
2017-03-03 10:25:03 -0600

    adding questions data

---
+++ skill/data/questions.js
@@ -0,0 +1,88 @@
+'use strict';
+module.exports = {
+    'QUESTIONS_EN_US' : [
+        {
+            'When was the first World Cup?' : [
+                '1940',
+                '1900',
+                '1944',
+                '1840',
+                '1920'
+            ]
+        },
+        {
+            ' Wich is the selection that has participated the most in the World Cups?' : [
+                'Brazil',
+                'Germany',
+                'Italy',
+                'Argentina',
+                'Nicaragua'
+            ]
+        },
+        {
+            'Which one of the followings never have been on a World Cup?' : [
+                'Nicaragua',
+                'El Salvador',
+                'Honduras',
+                'Czech republic'
+            ]
+        },
+        {
+            'Who is the World Cup top scorer?' : [
+                'Miroslav Klose',
+                'Luis Ronaldo',
+                'Pele',
+                'Gabriel Batistuta'
+            ]
+        },
+        {
+            'Which team had more players with golden balls?' : [
+                'AC Milan',
+                'Real Madrid',
+                'Barcelona',
+                'Manchester United'
+            ]
+        },
+        {
+            'Which selections had won the most world cup?' : [
+                'Brazil',
+                'Germany',
+                'Italy',
+                'Argentina',
+                'Uruguay'
+            ]
+        },
+        {
+            'Who wins the golden ball in 2004?' : [
+                'Andriy Shevchenko',
+                'Luis Figo',
+                'Michael Owen',
+                'Ronaldinho Gaucho'
+            ]
+        },
+        {
+            'Who wins the golden ball in 2007?' : [
+                'Ricardo Kaká',
+                'Andriy Shevchenko',
+                'Lionel Messi',
+                'Cristiano Ronaldo'
+            ]
+        },
+        {
+            'Who was named as the best player in the world in 2002?' : [
+                'Luis Ronaldo',
+                'Michael Owen',
+                'Rivaldo',
+                'Zinedine Zidane'
+            ]
+        },
+        {
+            'Who is considered the best defender ever in all times?' : [
+                'Paolo Maldini',
+                'Fabio Cannavaro',
+                'Stephan Effemberg',
+                'Roberto Carlos'
+            ]
+        }
+    ]
+};
